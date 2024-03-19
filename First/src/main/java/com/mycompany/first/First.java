e906057681e3b236de1d81bd0d6a5ceea073f4d1
vladislav245 <69549451+vladislav245@users.noreply.github.com>
2024-03-19 15:05:59 +0300

    Add files via upload

---
+++ First/src/main/java/com/mycompany/first/First.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.first;
+
+/**
+ *
+ * @author влад
+ */
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
